e46a532a72d8bf258fbf1f05de236a1c05922a62
Pete Michaud <michaudp@gmail.com>
2013-09-08 20:55:05 -0500

    added qualified page title syntax

--- _plugins/jekyllpedia.rb
+++ _plugins/jekyllpedia.rb
@@ -34,16 +34,30 @@ module Jekyll
     def transform(content)
       content.gsub WIKI_REGEX do |_|
         title = $~[:title]
+        matching_pages, title = get_page_from_title(title)
         anchor = $~[:anchor] || title
-        matching_pages = get_page_from_title(title)
         render_anchor(anchor, matching_pages)
       end
     end
 
     private
 
+    # Finds the page or pages that have the given title
+    #
+    # The title might be simple like "Page Title", in
+    # which case this method looks through all pages.
+    # The title might also be qualified like
+    # "category::subcategory::Page Title". In that
+    # case, this method ensures that it only returns
+    # pages under the "category" and "subcategory"
+    # folders
+    #
     def get_page_from_title(title)
-      @site.pages.select { |page| page.name == title }
+      qualified_title = title.split('::')
+
+      return @site.pages.select { |page|
+        page.name == qualified_title.last && under_directory(page.path, qualified_title[0...-1])
+      }, qualified_title.last
     end
 
     def render_anchor(title, pages)
@@ -78,9 +92,37 @@ module Jekyll
 
     def matches_directory(file_path)
       dir = File.dirname(file_path)
-      (@config[:directories] || [dir_strip(dir)]).select do |d|
+      directory_array = @config[:directories] || [dir_strip(dir)]
+      directory_array.select do |d|
         dir =~ dir_regex(d)
       end.size > 0
+    end
+
+    # Takes a file path and an array of directories, and determines
+    # if the file is "loosely" under those directories.
+    # For example:
+    #
+    # under_directory('path/to/file.md', %w(path to)) #=> true
+    # under_directory('path/to/file.md', %w(other_path)) #=> false
+    # under_directory('path/leading/to/file.md', %w(leading)) #=> true
+    # under_directory('path/leading/to/file.md', %w(path to)) #=> true
+    # under_directory('path/leading/to/file.md', %w(to path)) #=> false
+    #
+    def under_directory(file_path, directory_array)
+      dir = File.dirname(file_path)
+
+      directory_array.each do |d|
+
+        match = dir_regex(d).match(dir)
+
+        if match
+          dir = dir[dir.index(match.to_s)+match.to_s.size..-1]
+        else
+          return false
+        end
+      end
+
+      return true
     end
 
     def dir_strip(dir)
